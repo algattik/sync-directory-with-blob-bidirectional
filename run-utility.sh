a0fd529faa58eefb896c55cbf13ae572b52d40d7
algattik <algattik@microsoft.com>
2019-07-09 09:07:20 +0200

    Simplified logic

--- run-utility.sh
+++ run-utility.sh
@@ -3,48 +3,48 @@
 # Strict mode, fail on any error
 set -euo pipefail
 
-export_process() {
+list_blobs() {
 	marker=""
 	resultsPerPage=30
 	blobList=$(mktemp)
 	azStdErr=$(mktemp)
-	echo "[Utility] Querying export container"
+	echo "[Utility] Querying export container" >&2
 	while true; do
- 		az storage blob list --connection-string "$AZURE_STORAGE_CONNECTION_STRING" --container "$EXPORT_CONTAINER" --num-results $resultsPerPage --include m --query '[].[name, metadata.syncertag]' -o tsv --marker "$marker" >>$blobList 2>$azStdErr
+ 		az storage blob list --connection-string "$AZURE_STORAGE_CONNECTION_STRING" --num-results $resultsPerPage --marker "$marker" "$@" >>$blobList 2>$azStdErr
+		if grep error $azStdErr; then cat $azStdErr >&2; exit 98; fi
  		marker=$((grep -A1 -x 'WARNING: Next Marker:' "$azStdErr" || true) | sed '2q;d' | cut -f2 -d ' '); test -n "$marker" || break
- 		echo "[Utility] Querying next page of results..."
+ 		echo "[Utility] Querying next page of results..." >&2
  	done
+	rm $azStdErr
+	echo $blobList
+}
 	
-	filesInExportContainer=$(mktemp)
-	filesInExportFolder=$(mktemp)
-	filesDiff=$(mktemp)
-	cut -f1 $blobList | LC_ALL=C sort > $filesInExportContainer
-	ls $EXPORT_FOLDER | LC_ALL=C sort > $filesInExportFolder
-	(diff $filesInExportContainer $filesInExportFolder || true) | (grep '< ' || true) | sed 's/^..//' > $filesDiff
-	
-	blobsWithTag=$(mktemp)
-	(grep '1$' $blobList || true) | cut -f1 > $blobsWithTag
+export_process() {
+	blobList=$(list_blobs --container "$EXPORT_CONTAINER" --include m --query '[].[name,metadata.syncertag]' -o tsv)
 	
 	local IFS=$'\n'
-	for f in $(cat $filesDiff); do
-		if ! grep -q -F -x "$f" $blobsWithTag ; then
+	for lin in $(cat $blobList); do
+		f=$(cut -f1 <<< "$lin")
+		tag=$(cut -f2 <<< "$lin")
+		if test -e "$EXPORT_FOLDER/$f"; then continue; fi
+		if [ "$tag" != "1" ]  ; then
 			# new blob
-			echo "[Utility] Downloading $f"
+			echo "[Utility] Downloading $f" >&2
 			az storage blob download --connection-string "$AZURE_STORAGE_CONNECTION_STRING" --container-name "$EXPORT_CONTAINER" --name "$f" --file "$EXPORT_FOLDER/$f" -o none
 			az storage blob metadata update --connection-string "$AZURE_STORAGE_CONNECTION_STRING" --container-name "$EXPORT_CONTAINER" --name "$f" --metadata "syncertag=1" -o none
 		else
 			# blob to be deleted
-			echo "[Utility] Deleting $f"
+			echo "[Utility] Deleting $f" >&2
 			az storage blob delete --connection-string "$AZURE_STORAGE_CONNECTION_STRING" --container-name "$EXPORT_CONTAINER" --name "$f" -o none
 		fi
 	done
 
-	rm $blobList $azStdErr $filesInExportContainer $filesInExportFolder $filesDiff $blobsWithTag
+	rm $blobList
 }
 
-echo "[Utility] Testing that sudo works"
+echo "[Utility] Testing that sudo works" >&2
 if ! sudo -nv; then
-	echo "[Utility] FATAL: sudo is required"
+	echo "[Utility] FATAL: sudo is required" >&2
 	exit 1
 fi
 
@@ -58,23 +58,23 @@ import_process() {
 		[ -f "$filename" ] || continue
 		basename=$(basename "$filename")
 		delete_status=$(az storage blob list --connection-string "$AZURE_STORAGE_CONNECTION_STRING" --container "$IMPORT_CONTAINER" --prefix "$basename" --num-results '*' --include d | jq ".[] | select (.name==\"$basename\").deleted")
-		echo "[Utility] ($basename) -> ($delete_status)"
+		echo "[Utility] ($basename) -> ($delete_status)" >&2
 
 		case "delete_status_$delete_status" in
         		delete_status_) #new file
             			if ! is_file_being_written "$filename"; then
-					echo "[Utility] Uploading $basename"
+					echo "[Utility] Uploading $basename" >&2
 					az storage blob upload --connection-string "$AZURE_STORAGE_CONNECTION_STRING" --container-name "$IMPORT_CONTAINER" --name "$basename" --file "$filename" -o none
 				else
-					echo "[Utility] Ignoring $basename, concurrent process is writing"
+					echo "[Utility] Ignoring $basename, concurrent process is writing" >&2
 				fi
             		;;
         		delete_status_true) #deleted blob
-				echo "[Utility] Deleting local file $basename"
+				echo "[Utility] Deleting local file $basename" >&2
 				rm "$filename"
             		;;
         		delete_status_false) #already copied file
-				echo "[Utility] Ignoring already copied file $basename"
+				echo "[Utility] Ignoring already copied file $basename" >&2
             		;;
         		*) #unexpected jq output
 				echo "[Utility] Unexpected jq output: $delete_status" >&2
